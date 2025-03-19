3a9198751b1577b4243ba3e50432d0a1e54ba969
OKA <eokhwa222@gmail.com>
2025-03-19 21:32:25 +0900

    FIRST

---
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -0,0 +1,32 @@
+package jpabook.jpashop.domain;
+
+import jakarta.persistence.*;
+import jpabook.jpashop.domain.item.Item;
+import lombok.Getter;
+import lombok.Setter;
+
+import static jakarta.persistence.FetchType.LAZY;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "order_item")
+public class OrderItem {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "order_item_id")
+    private Long id;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "itme_id")
+    private Item item;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    private  int orderPrice;
+    private int count;
+
+}
